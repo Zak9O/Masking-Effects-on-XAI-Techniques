fda2c53001fec361258624f47d1f6ec1fce7a206
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-11-13 21:45:16 +0100

    Changed HPC runtime on anonymizaiton

--- src/hpc/LSF_options.sh
+++ src/hpc/LSF_options.sh
@@ -7,12 +7,9 @@
 # --- Ask for number of cores (default: 1) --
 #BSUB -n 1
 # --- Specify that we need 4GB of memory per core/slot --
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=1GB]"
 
 # --- Set walltime limit: hh:mm --
-# NOTE: Your LIME calculation with 5000 explanations might take a long time.
-# I've set this to 1 hour (1:00) as a test.
-# You may need to increase this significantly (e.g., to 24:00 or more).
 #BSUB -W 8:00
 
 
