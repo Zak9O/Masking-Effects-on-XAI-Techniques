ca0abf80f7b0bad703e2703601206a57078547c2
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-11-26 22:45:50 +0100

    Created new quantile hierarchy generator

--- src/masking_effects_on_xai_techniques/hierarchies.py
+++ src/masking_effects_on_xai_techniques/hierarchies.py
@@ -2,11 +2,50 @@ import pandas as pd
 import numpy as np
 
 
-def save_hierarchy(hierarchy: dict[int, pd.Series], path: str):  # pyright: ignore[reportRedeclaration]
+def save_hierarchy(hierarchy: dict[int, pd.Series], path: str, sort=True):  # pyright: ignore[reportRedeclaration]
     hierarchy: pd.DataFrame = pd.DataFrame(hierarchy)
-    hierarchy = hierarchy.sort_values(by=0)  # pyright: ignore[reportArgumentType]
-    hierarchy = hierarchy.drop_duplicates(subset=[0])
+    if sort:
+        hierarchy = hierarchy.sort_values(by=0)  # pyright: ignore[reportArgumentType]
+        hierarchy = hierarchy.drop_duplicates(subset=[0])
     hierarchy.to_csv(path, index=False, header=False)
+
+
+def generate_qcut_hierarchy(values: pd.Series, levels: int) -> dict[int, pd.Series]:
+    unique = values.copy().drop_duplicates().sort_values()
+
+    hierarchy: dict[int, pd.Series] = {}
+
+    hierarchy[0] = unique
+
+    for i in range(1, levels - 1):
+        bins = _generate_bins(unique, levels - i)
+        current_level_hierarchy = []
+
+        for j in unique.index:
+            current_level_hierarchy.append(bins[j])
+
+        hierarchy[i] = pd.Series(current_level_hierarchy, index=unique.index)
+        pass
+
+    hierarchy[levels - 1] = pd.Series("*", index=unique.index)
+
+    return hierarchy
+
+
+def _generate_bins(values: pd.Series, levels: int) -> pd.Series:
+    n = levels
+    n_old = levels
+    while True:
+        bins = pd.qcut(values, n, duplicates="drop")
+        bins_len = bins.nunique()
+        if bins_len == levels:
+            break
+        elif bins_len < levels:
+            n_old = n
+            n *= 2
+        elif bins_len > levels:
+            n = n - int((n - n_old) / 2)
+    return bins
 
 
 def generate_hierarchy(values: pd.Series, levels: int) -> dict[int, pd.Series]:
