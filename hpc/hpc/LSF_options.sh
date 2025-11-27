f20fe3746c866f33e15d7e78b2695ccf6404384f
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-11-27 22:50:25 +0100

    Moved hpc and migrated to uv

---
+++ hpc/hpc/LSF_options.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+### General LSF options
+# --- Specify the queue --
+#BSUB -q hpc
+# --- Set the job Name --
+#BSUB -J ${JOB_NAME}
+# --- Ask for number of cores (default: 1) --
+#BSUB -n ${LSB_NCPU}
+#BSUB -R "span[hosts=1]"
+# --- Specify that we need 4GB of memory per core/slot --
+#BSUB -R "rusage[mem=${LSB_MEM}]"
+
+# --- Set walltime limit: hh:mm --
+# NOTE: Your LIME calculation with 5000 explanations might take a long time.
+# I've set this to 1 hour (1:00) as a test.
+# You may need to increase this significantly (e.g., to 24:00 or more).
+#BSUB -W ${LSB_TIME_H}:00
+
+
+# --- Specify the output and error file. %J is the job-id --
+#BSUB -o job_out/${JOB_NAME}_%J.out
+#BSUB -e job_out/${JOB_NAME}_%J.err
+
+# --- End of LSF options --
---
+++ hpc/scripts/test.py
@@ -0,0 +1,4 @@
+import masking_effects_on_xai_techniques.utils as utils
+
+
+print(utils.common_elements([[1, 2], [2, 3]]))
