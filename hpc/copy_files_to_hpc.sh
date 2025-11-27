0cc87f982b5750266e2d65b738c87969dd744cbf
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-11-27 23:04:48 +0100

    hpc dump

--- hpc/copy_files_to_hpc.sh
+++ hpc/copy_files_to_hpc.sh
@@ -5,10 +5,7 @@ source ./common_vars.sh
 rm -rf ./hpc
 mkdir ./hpc
 
-cp ./common_vars.sh ./LSF_options.sh ./LSP_run.sh ./README.md ./requirements.txt ./setup.sh ./submit.sh ./hpc/
-
-cp -r ./data ./anonymize.py ./hierarchies/ ./hpc
+cp ./common_vars.sh ./LSF_options.sh ./LSF_run.sh ./README.md ./pyproject.toml ./setup.sh ./submit.sh ./hpc/
+cp -r ./scripts/ ./hpc
 
 scp -i ~/.ssh/id_ed25519 -r hpc s225169@transfer.gbar.dtu.dk:
-
-rm -rf ./hpc
