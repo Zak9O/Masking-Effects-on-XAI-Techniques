f75f4e7bcb64c6d4d88feb1ac5ff5986ef27da48
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-12-03 22:25:13 +0100

    Refactor copy

--- hpc/copy_from_hpc.sh
+++ hpc/copy_from_hpc.sh
@@ -5,8 +5,12 @@ scp -r -i ~/.ssh/id_ed25519 "s225169@transfer.gbar.dtu.dk:~/explanation/out/*" t
 datasets=("adult" "usa_house" "cervic_cancer")
 
 for dataset in "${datasets[@]}"; do
-  cp -r "tmp/lime/data/${dataset}/*" "../data/${dataset}/lime/"
-  cp -r "tmp/shap/data/${dataset}/*" "../data/${dataset}/shap/"
+
+  rm -rf ../data/${dataset}/lime/ ../data/${dataset}/shap/
+  mkdir ../data/${dataset}/lime/
+  mkdir ../data/${dataset}/shap/
+  cp -r "./tmp/lime/data/${dataset}/"* "../data/${dataset}/lime/"
+  cp -r "./tmp/shap/data/${dataset}/"* "../data/${dataset}/shap/"
 done
 
 rm -rf tmp
