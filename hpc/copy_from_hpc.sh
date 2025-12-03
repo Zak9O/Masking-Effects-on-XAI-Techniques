60ea38c819e89390356edd797527262bbf607bfc
Casper Lauge Nørup Koch <kochcasper@gmail.com>
2025-12-03 21:51:16 +0100

    Created copy job

---
+++ hpc/copy_from_hpc.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+mkdir -p tmp
+scp -r -i ~/.ssh/id_ed25519 "s225169@transfer.gbar.dtu.dk:~/anonymization/out/*" tmp/
+
+for dataset_folder in tmp/*; do
+    if [ -d "$dataset_folder" ]; then
+        dataset_name=$(basename "$dataset_folder")
+        
+        for method_folder in "$dataset_folder"/*; do
+            if [ -d "$method_folder" ]; then
+                method_name=$(basename "$method_folder")
+                
+                target_path="../data/$dataset_name/$method_name"
+                
+                if [ -d "$target_path" ]; then
+                    echo "Deleting existing $target_path"
+                    rm -rf "$target_path"
+                fi
+                
+                echo "Moving $method_folder to ../data/$dataset_name/"
+                mv "$method_folder" "../data/$dataset_name/"
+            fi
+        done
+    fi
+done
+
+rm -rf tmp
+
